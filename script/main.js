e25de0cdf4f4f8fb9bc520ee66255ffead0e9447
VolodymyrLiubashenko <vovalubashenko@gmail.com>
2021-09-21 12:38:23 +0300

    burger

--- script/main.js
+++ script/main.js
@@ -1,5 +1,14 @@
-console.log(window.screen.width)
+let burger = document.getElementById('burger')
+let navigationMenuContainer = document.getElementById('navigation_menu_container')
+
+let toggleActiveClassToNavMenu = ()=>{
+   navigation_menu_container.classList.toggle('active')
+   burger.classList.toggle('active')
+}
+burger.addEventListener('click', toggleActiveClassToNavMenu)
+
 $(document).ready(function(){
    if(window.screen.width <= 520)
       $('.clients_icons').slick()
 })
+
